9f14ea30c71ce0c146f7d9f3ca27b33316eaa1b7
GANIDU2413 <ganidusahan@gmail.com>
2024-03-04 11:42:42 +0530

    updated

--- src/DatagramPack.java
+++ src/DatagramPack.java
@@ -7,10 +7,9 @@ public class DatagramPack {
             DatagramSocket dataSock = new DatagramSocket();
             String str = "Hello Server! this is Datagram";
             InetAddress address = InetAddress.getByName("127.0.0.1");
-            DatagramPacket dpack = new DatagramPacket(str.getBytes(),str.length(),address,9091);
+            DatagramPacket dpack = new DatagramPacket(str.getBytes(),str.length(),address,9008);
 
             dataSock.send(dpack);
-            dataSock.close();
 
         } catch (SocketException e) {
             System.out.println(e.getMessage());
--- src/DatagramSock.java
+++ src/DatagramSock.java
@@ -5,14 +5,14 @@ import java.net.SocketException;
 public class DatagramSock {
     public static void main(String[] args) {
         try {
-            DatagramSocket dsock = new DatagramSocket(9091);
+            DatagramSocket dsock = new DatagramSocket(9008);
             byte[] buff = new byte[1024];
             DatagramPacket datapack = new DatagramPacket(buff,1024);
+            dsock.receive(datapack);
             String str = new String(datapack.getData(),0,datapack.getLength());
             System.out.println(str);
-            dsock.close();
 
-        } catch (SocketException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
